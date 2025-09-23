2d1360a0deac79ea6ec806ddd308a9e984a8ca1b
santiago-ondris <santiagonicolas2001@gmail.com>
2025-09-23 16:15:37 -0300

    ultimas ideas

--- Fulbito.Api/Program.cs
+++ Fulbito.Api/Program.cs
@@ -131,6 +131,9 @@ builder.Services.AddAdminLeagueFeatures();
 builder.Services.AddViewMatchupsFeatures();
 builder.Services.AddUpdatePlayerImageFeatures();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+
 var app = builder.Build();
 
 if (app.Environment.IsProduction())
