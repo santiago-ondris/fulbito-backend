d2995bbb69ce0f36d19767bff190aa6921a61232
santiago-ondris <santiagonicolas2001@gmail.com>
2025-09-25 10:25:13 -0300

    migraciones añadidas

---
+++ Fulbito.Core/Migrations/20250916154439_AddMvpFeature.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fulbito.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMvpFeature : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMvp",
+                table: "PlayerMatches",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsMvpEnabled",
+                table: "Leagues",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PointsPerMvp",
+                table: "Leagues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsMvp",
+                table: "PlayerMatches");
+
+            migrationBuilder.DropColumn(
+                name: "IsMvpEnabled",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "PointsPerMvp",
+                table: "Leagues");
+        }
+    }
+}
---
+++ Fulbito.Core/Migrations/20250916185309_Streak.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fulbito.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Streak : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MinLossStreakToActivate",
+                table: "Leagues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinWinStreakToActivate",
+                table: "Leagues",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MinLossStreakToActivate",
+                table: "Leagues");
+
+            migrationBuilder.DropColumn(
+                name: "MinWinStreakToActivate",
+                table: "Leagues");
+        }
+    }
+}
